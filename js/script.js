2a0b37f05baad28a463d250a4f3061f186777d52
Bori-github <85009583+Bori-github@users.noreply.github.com>
2021-12-15 21:51:17 +0900

    Add Javascript

--- js/script.js
+++ js/script.js
@@ -1,18 +1,64 @@
 const btnLang = document.querySelector(".btn-lang");
 const listLang = document.querySelector(".list-lang");
 
-const btnCombo = document.querySelector(".btn-combo");
-const listCombo = document.querySelector(".btn-combo + ul");
+const btnCombo = document.querySelectorAll(".btn-combo");
+const listCombo = document.querySelectorAll(".list-combo ul");
+const comboOption = document.querySelectorAll(".combo-option");
+const btnNext = document.querySelector(".btn-next");
 
 const btnSite = document.querySelector(".btn-site");
 const listSite = document.querySelector(".list-site");
 
-btnLang.addEventListener("click", () => {
-  listLang.classList.toggle("show");
+let arrowDown = true;
+function showList() {
+  // console.log(this.parentElement);
+  const thisList = this.parentElement.querySelector("ul");
+  const arrowIcon = this.querySelector("span:last-child");
+
+  if (arrowDown) {
+    arrowIcon.innerText = "keyboard_arrow_up";
+    arrowDown = false;
+  } else {
+    arrowIcon.innerText = "keyboard_arrow_down";
+    arrowDown = true;
+  }
+
+  thisList.classList.toggle("show");
+}
+
+btnLang.addEventListener("click", showList);
+
+btnCombo.forEach((el) => {
+  el.addEventListener("click", showList);
 });
-btnCombo.addEventListener("click", () => {
-  listCombo.classList.toggle("show");
+
+// listCombo.forEach((el) => {
+//   el.addEventListener("click", (item) => {
+//     // console.log();
+//     // console.log(item.target.innerText);
+//     console.log(item);
+
+//     // console.log(item.path[3].innerText);
+//     // let itemCombo = item.target;
+//     // itemCombo.classList.toggle("selected");
+//     // btnNext.classList.toggle("selected");
+//   });
+// });
+
+comboOption.forEach((el) => {
+  el.addEventListener("click", (item) => {
+    console.log(this);
+    // console.log(item.target.innerText);
+    // console.log(item.path);
+    // console.log(item.path[3].innerText);
+    let itemCombo = item.target;
+    itemCombo.classList.toggle("selected");
+    btnNext.classList.toggle("selected");
+  });
 });
-btnSite.addEventListener("click", () => {
-  listSite.classList.toggle("show");
-});
+
+btnSite.addEventListener("click", showList);
+
+// document.addEventListener("scroll", function (e) {
+//   console.log(e);
+// });
